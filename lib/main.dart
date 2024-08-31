dabc0f51fc5a4f9892b9eafc3da3a4ac10eea073
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-08-31 18:28:09 -0400

    Initialize Flutter app and add flutter_riverpod
    - Created Flutter project and added 'flutter_riverpod' version 2.5.1.
    - Cleaned up boilerplate code
    - Ran 'flutter pub get' to update dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-          useMaterial3: true,
-        ),
-        home: const HomePage());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
