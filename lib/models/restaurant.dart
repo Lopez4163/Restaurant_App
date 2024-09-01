9735c8723d1243d44823abce7ad432863be1bbda
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-09-01 18:29:49 -0400

    - added new elements to JSON file    - updated Restaurant model with new elements   - this will be used in the details_screen

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -3,19 +3,27 @@ class Restaurant {
   final String name;
   final String cuisine;
   final String image;
+  final String slogan;
+  final String phone;
+  final String address;
 
-  Restaurant({
-    required this.id,
-    required this.name,
-    required this.cuisine,
-    required this.image,
-  });
+  Restaurant(
+      {required this.id,
+      required this.name,
+      required this.cuisine,
+      required this.image,
+      required this.slogan,
+      required this.phone,
+      required this.address});
 
   factory Restaurant.fromJson(Map<String, dynamic> json) {
     return Restaurant(
         id: json['id'],
         name: json['name'],
         cuisine: json['cuisine'],
-        image: json['image']);
+        image: json['image'],
+        slogan: json['slogan'],
+        phone: json['phone'],
+        address: json['address']);
   }
 }
