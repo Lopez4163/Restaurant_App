1d2ed4fb5d718534125390c1128d94f51876b7db
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-09-01 17:04:57 -0400

    - updated the restaurant model with the new image element

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -2,14 +2,20 @@ class Restaurant {
   final int id;
   final String name;
   final String cuisine;
+  final String image;
 
-  Restaurant({required this.id, required this.name, required this.cuisine});
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.cuisine,
+    required this.image,
+  });
 
   factory Restaurant.fromJson(Map<String, dynamic> json) {
     return Restaurant(
-      id: json['id'],
-      name: json['name'],
-      cuisine: json['cuisine'],
-    );
+        id: json['id'],
+        name: json['name'],
+        cuisine: json['cuisine'],
+        image: json['image']);
   }
 }
